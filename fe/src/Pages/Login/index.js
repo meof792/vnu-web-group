2e1bf9ce8d30e9f7991a3ef2345f6613de1c4e97
Pham The Phong <emyeugay12345@gmail.com>
2024-07-15 17:55:28 +0700

    fix(login)

--- fe/src/Pages/Login/index.js
+++ fe/src/Pages/Login/index.js
@@ -1,12 +1,10 @@
 import classNames from "classnames";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Context from "./Context";
 import { faCheck, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import "./Login.scss";
-// add code
-// add code
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -17,13 +15,12 @@ function Login() {
   const [inputType, setInputType] = useState("password");
   const [error, setError] = useState("");
 
-  useEffect(() => {}, [acceptTerms]);
   const handleLogin = async (e) => {
     e.preventDefault();
     setShowPassword(false);
     setShowName(false);
     setError("");
-    // Nếu tất cả điều kiện đều đúng, tiếp tục gửi yêu cầu đăng nhập
+
     try {
       const response = await axios.post("http://127.0.0.1:8000/api/login", {
         username: email,
@@ -44,38 +41,22 @@ function Login() {
         } else {
           setShowName(false);
           setShowPassword(true);
-      
-      if(acceptTerms){
-        if (response.data.success) {
-            localStorage.setItem("username", email); // Lưu tên người dùng đã đăng nhập toàn cục
-            alert("Đăng nhập thành công!");
-        } else {
-          if (response.data.type === "tk") {
-            setShowName(true);
-            setShowPassword(false);
-          } else {
-            setShowName(false);
-            setShowPassword(true);
-          }
-          setError(response.data.msg);
         }
-      }else{
-        alert("Bạn chưa xác nhận!");
+        setError(response.data.msg); // Đặt thông báo lỗi nếu có
       }
     } catch (error) {
       console.error("Có lỗi xảy ra!", error);
     }
   };
-  console.log(acceptTerms + "checkbox");
+
   return (
     <div className="wrapper w-full block justify-between md:flex md:mt-0 py-[20px] px-[20px]">
       {/* content */}
-
       <Context />
       {/* login */}
-      <div className="  items-center max-h-[450px]  md:w-[700px] mt-20 md:m-0 border-2 border-[#ccc]">
-        <div className=" flex items-center pl-6 py-5 bg-green_400 text-white border-b-2 border-[#ccc] ">
-          <span className="sm:text-4xl text-2xl ">Đăng Nhập</span>
+      <div className="items-center max-h-[450px] md:w-[700px] mt-20 md:m-0 border-2 border-[#ccc]">
+        <div className="flex items-center pl-6 py-5 bg-green_400 text-white border-b-2 border-[#ccc]">
+          <span className="sm:text-4xl text-2xl">Đăng Nhập</span>
         </div>
         <form className="px-6 py-10" onSubmit={handleLogin}>
           <label className="sm:text-3xl text-2xl">Tên truy cập</label>
@@ -86,11 +67,10 @@ function Login() {
             )}
           >
             <input
-              className="sm:text-3xl text-2xl "
+              className="sm:text-3xl text-2xl"
               type="text"
               autoFocus
               placeholder="Mã sinh viên"
-              autoFocus
               value={email || ""}
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -114,12 +94,11 @@ function Login() {
             <input
               className="sm:text-3xl text-2xl"
               type={inputType}
-              type="password"
+              autoFocus
               placeholder="Mật khẩu"
               value={password || ""}
               onChange={(e) => setPassword(e.target.value)}
             />
-
             <FontAwesomeIcon
               onMouseEnter={() => setInputType("text")}
               onMouseLeave={() => setInputType("password")}
@@ -134,8 +113,8 @@ function Login() {
           </div>
           <div className="flex items-center justify-between">
             <span className="sm:text-2xl text-xl text-green_400">
-              Quên mật khẩu ?
-            </span>{" "}
+              Quên mật khẩu?
+            </span>
             <button
               className="w-[100px] h-[40px] bg-green_400 text-white text-2xl hover:scale-110 hover:bg-green_400/70 transition-all"
               type="submit"
@@ -143,14 +122,18 @@ function Login() {
               Đăng Nhập
             </button>
           </div>
-          <div className=" relative bg-[#F5F5F5] p-5 mt-10 flex items-center">
+          <div className="relative bg-[#F5F5F5] p-5 mt-10 flex items-center">
             <input
-              className=" opacity-0 z-10 border-2 w-[20px] h-[20px] bg-white text-[#ccc] mr-8 "
+              className="opacity-0 z-10 border-2 w-[20px] h-[20px] bg-white text-[#ccc] mr-8"
               type="checkbox"
               checked={acceptTerms}
               onChange={(e) => setAcceptTerms(e.target.checked)}
             />
-            <div className="absolute top-1/2 left-5 transform bg-white -translate-y-1/2 w-[20px] h-[20px]  flex items-center justify-center">
+            <div
+              className={classNames(
+                "absolute top-1/2 left-5 bg-white transform -translate-y-1/2 w-[20px] h-[20px] flex items-center justify-center"
+              )}
+            >
               {acceptTerms ? (
                 <FontAwesomeIcon
                   icon={faCheck}
